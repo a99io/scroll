831347bf8cce9be637f010f7a452402b974d04f4
a99io <aliabdulameer575@gmail.com>
2025-01-29 03:05:31 +0300

    new-batch

---
+++ index.js
@@ -0,0 +1,26 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let nouvil = document.querySelector('.nouvil');
+window.onscroll = function(){
+    let value=scrollY;
+    stars.style.left= value +'px';
+    moon.style.top= value*4 +'px';
+    mountains3.style.top= value*2 +'px';
+    mountains4.style.top= value*1.5 +'px';
+    river.style.top= value +'px';
+    boat.style.top= value +'px';
+    boat.style.left= value*3 +'px';
+    nouvil.style.fontSize = value +'px';
+    if ( scrollY >=69) {
+        nouvil.style.fontSize = 67 +'px';
+        nouvil.style.position = 67 +'fixed';
+    }
+    if (scrollY >=97){
+        document.querySelector('.main').style.background='linear-gradient(#376281,#10001f);'
+    }
+    
+}
